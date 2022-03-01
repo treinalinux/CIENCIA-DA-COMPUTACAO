8dbfa21e9e4061f5c937b4d6ebc4ecfe56c7698a
Alan da Silva Alves <linuxfama@gmail.com>
2022-02-28 23:15:29 -0300

    feat: writer on files

---
+++ Segundo Período/Programação Extrema/Files/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Inoforme o numero de linhas do arquivo: ");
+        int numeroLinhas = scanner.nextInt();
+
+        FileWriter arquivo = new FileWriter("teste.txt");
+        PrintWriter writer = new PrintWriter(arquivo);
+
+        for (int i=1; i <= numeroLinhas; i++) {
+            writer.printf("Esta é a linha de número " + i + "%n");
+        }
+
+        arquivo.close();
+        System.out.printf("Arquivo escrito com sucesso!\n");
+    }
+}
